b072a04b555dee1300b87750875f315050f74b5c
James Reategui <JamesReate@users.noreply.github.com>
2024-05-14 12:32:48 -0400

    add tests to validate functionality

---
+++ internal/models/template_configs_test.go
@@ -0,0 +1,107 @@
+package models
+
+import "testing"
+
+func TestPIDRequest_FormulaType(t *testing.T) {
+	type fields struct {
+		Formula              string
+		Header               uint32
+		IntervalSeconds      int
+		Mode                 uint32
+		Name                 string
+		Pid                  uint32
+		Protocol             string
+		CanflowControlClear  bool
+		CanFlowControlIDPair string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   FormulaType
+	}{
+		{
+			name: "dbc formula type",
+			fields: fields{
+				Formula: `dbc:31|8@0+ (1,-40) [-40|215] "Celcius"`,
+			},
+			want: Dbc,
+		},
+		{
+			name: "python formula type",
+			fields: fields{
+				Formula: `python:(bytes_to_int(messages[0].data[-1:]) - 50) * 1.8 + 32`,
+			},
+			want: Python,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PIDRequest{
+				Formula:              tt.fields.Formula,
+				Header:               tt.fields.Header,
+				IntervalSeconds:      tt.fields.IntervalSeconds,
+				Mode:                 tt.fields.Mode,
+				Name:                 tt.fields.Name,
+				Pid:                  tt.fields.Pid,
+				Protocol:             tt.fields.Protocol,
+				CanflowControlClear:  tt.fields.CanflowControlClear,
+				CanFlowControlIDPair: tt.fields.CanFlowControlIDPair,
+			}
+			if got := p.FormulaType(); got != tt.want {
+				t.Errorf("FormulaType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPIDRequest_FormulaValue(t *testing.T) {
+	type fields struct {
+		Formula              string
+		Header               uint32
+		IntervalSeconds      int
+		Mode                 uint32
+		Name                 string
+		Pid                  uint32
+		Protocol             string
+		CanflowControlClear  bool
+		CanFlowControlIDPair string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "dbc formula type",
+			fields: fields{
+				Formula: `dbc:31|8@0+ (1,-40) [-40|215] "Celcius"`,
+			},
+			want: `31|8@0+ (1,-40) [-40|215] "Celcius"`,
+		},
+		{
+			name: "python formula type",
+			fields: fields{
+				Formula: `python:(bytes_to_int(messages[0].data[-1:]) - 50) * 1.8 + 32`,
+			},
+			want: `(bytes_to_int(messages[0].data[-1:]) - 50) * 1.8 + 32`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PIDRequest{
+				Formula:              tt.fields.Formula,
+				Header:               tt.fields.Header,
+				IntervalSeconds:      tt.fields.IntervalSeconds,
+				Mode:                 tt.fields.Mode,
+				Name:                 tt.fields.Name,
+				Pid:                  tt.fields.Pid,
+				Protocol:             tt.fields.Protocol,
+				CanflowControlClear:  tt.fields.CanflowControlClear,
+				CanFlowControlIDPair: tt.fields.CanFlowControlIDPair,
+			}
+			if got := p.FormulaValue(); got != tt.want {
+				t.Errorf("FormulaValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
